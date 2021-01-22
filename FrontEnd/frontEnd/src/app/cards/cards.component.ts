61ebe0784213cd0c8055842e43ce568f9064d5f1
Duangboudda <tongpan78@hotmail.com>
2021-01-22 08:16:08 -0800

    added cards component and card interface

--- FrontEnd/frontEnd/src/app/cards/cards.component.ts
+++ FrontEnd/frontEnd/src/app/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Card } from '../card';
 
 @Component({
   selector: 'app-cards',
@@ -6,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
+ card: Card = {
+  id: 1,
+  attactStat: 2,
+  defenseStat: 3,
+  inDeck: true,
+  cardClass: "yes",
+  cardName: 'Magic card'
 
+ };
+
+ cards: Card[];
   constructor() { }
 
   ngOnInit(): void {
