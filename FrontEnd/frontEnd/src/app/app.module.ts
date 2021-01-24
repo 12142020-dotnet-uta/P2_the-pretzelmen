05e7930d524a1f4cf7c17a3044af1ac1e459a08e
Duangboudda <tongpan78@hotmail.com>
2021-01-24 08:28:11 -0800

    added components

--- FrontEnd/frontEnd/src/app/app.module.ts
+++ FrontEnd/frontEnd/src/app/app.module.ts
@@ -4,15 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CardsComponent } from './cards/cards.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatchHistoryComponent } from './match-history/match-history.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PlayersComponent } from './players/players.component';
+import { CardDetailsComponent } from './card-details/card-details.component';
+import { CardCollectionComponent } from './card-collection/card-collection.component';
+import { MemoryGameComponent } from './memory-game/memory-game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardsComponent,
-    NavBarComponent
+    NavBarComponent,
+    MatchHistoryComponent,
+    PlayersComponent,
+    CardDetailsComponent,
+    CardCollectionComponent,
+    MemoryGameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
