dab9acf65fd26efeb8a0b1ae43f9d36451f52092
Duangboudda <tongpan78@hotmail.com>
2021-01-24 09:32:54 -0800

    added momery-game component

--- FrontEnd/frontEnd/src/app/app-routing.module.ts
+++ FrontEnd/frontEnd/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { MatchHistoryComponent } from './match-history/match-history.component';
 import { PlayersComponent} from './players/players.component'
 import { CardDetailsComponent} from './card-details/card-details.component';
 import { CardCollectionComponent } from './card-collection/card-collection.component';
+import { MemoryGameComponent} from './memory-game/memory-game.component';
 
 
 
@@ -12,7 +13,9 @@ const routes: Routes = [
  { path: 'matchHistory', component: MatchHistoryComponent},
  { path: 'players', component: PlayersComponent},
  { path: 'cardDetails/:id', component: CardDetailsComponent},
- { path: 'cardCollection', component: CardCollectionComponent}
+ { path: 'cardCollection', component: CardCollectionComponent},
+ { path: 'memoryGame', component: MemoryGameComponent}
+ 
 ];
 
 
--- FrontEnd/frontEnd/src/app/memory-game/memory-game.component.ts
+++ FrontEnd/frontEnd/src/app/memory-game/memory-game.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./memory-game.component.css']
 })
 export class MemoryGameComponent implements OnInit {
+  heroes: any[] = [1,2,3,4,5,6,7,8,9,10];
+
 
   constructor() { }
 
