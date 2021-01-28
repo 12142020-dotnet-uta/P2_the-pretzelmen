0f39155eebb8256734c69fb43d13bce614042ef2
tduangbo <tduangbo@asu.edu>
2021-01-28 07:01:52 -0800

    adde access -header for httpClient request

--- fronEnd/frontEnd/src/app/user/user.component.ts
+++ fronEnd/frontEnd/src/app/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import {UserService} from '../user.service'
+import {Observable} from "rxjs"
 
 @Component({
   selector: 'app-user',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  users: Observable<any[]>;
 
-  constructor() { }
+  constructor(private userService: UserService) { 
+    
+  }
 
-  ngOnInit(): void {
+  ngOnInit(): void  {
+    this.users = this.userService.getUsers();
   }
 
 }
