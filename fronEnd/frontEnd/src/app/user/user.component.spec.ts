4dc737ada4cd66483d20d8644384ee53c248104f
Tyler Cadena <tylercadena@alum.calarts.edu>
2021-01-28 18:11:01 -0800

    fixed testing but build now fails due to user.service.ts

--- fronEnd/frontEnd/src/app/user/user.component.spec.ts
+++ fronEnd/frontEnd/src/app/user/user.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { UserComponent } from './user.component';
+import { UserService } from '../user.service';
 
 describe('UserComponent', () => {
   let component: UserComponent;
@@ -8,6 +9,8 @@ describe('UserComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [ UserService ],
       declarations: [ UserComponent ]
     })
     .compileComponents();
