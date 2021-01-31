b1d3b38f04b0f92f31f57c29684f594c7f89e13f
tduangbo <tduangbo@asu.edu>
2021-01-31 11:05:33 -0800

    nn

--- FrontEnd/frontEnd/src/app/memory-action/memory-action.component.ts
+++ FrontEnd/frontEnd/src/app/memory-action/memory-action.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./memory-action.component.css']
 })
 export class MemoryActionComponent implements OnInit {
+  cards: any[] = [1,2,3,4,5,6,7,8,9,10];
+  actionName = 'Go for it';
+  isSpecial = true;
+  canSave = true;
+  classExpression = 'special clearance';
+  styleExpression = 'color: red';
+  color = 'blue';
+  num = 1;
+  flipCard = true;
+  cssString: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  clickedCard(num: number){
+    this.cssString = 'flip-card-inner';
+    this.num = num + 1;
+    this.flipCard = true;
+    console.log("CARD ID + " + this.num)
+    this.getClass();
+  }
+  getClass(){
+    /*
+    if(this.cssString.length > 3){
+      this.cssString = '';
+    }
+    */
+   
+    console.log("get class is called ");
+    return this.cssString;
+    //'flip-card-inner';
+  }
 }
