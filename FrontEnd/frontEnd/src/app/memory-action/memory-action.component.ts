dcbb1ea8577e0b6a7354541650505298de611ebf
tduangbo <tduangbo@asu.edu>
2021-01-31 15:21:47 -0800

    flipped

--- FrontEnd/frontEnd/src/app/memory-action/memory-action.component.ts
+++ FrontEnd/frontEnd/src/app/memory-action/memory-action.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./memory-action.component.css']
 })
 export class MemoryActionComponent implements OnInit {
-  cards: any[] = [1,2,3,4,5,6,7,8,9,10];
+  cards: any[] = [1,2,3,4,5,6];
   actionName = 'Go for it';
   isSpecial = true;
   canSave = true;
@@ -16,6 +16,9 @@ export class MemoryActionComponent implements OnInit {
   num = "";
   flipCard = true;
   cssString: string = '';
+  cardOne: string = '0';
+  cardTwo: string = '0';
+  numberFlip = 0;
 
   constructor() { }
 
@@ -23,15 +26,33 @@ export class MemoryActionComponent implements OnInit {
   }
   clickedCard(num: string){
     this.num = num;
-    console.log("class id is :  " + this.num);
+    
+    if(this.numberFlip == 2){
+      //console.log("previous clicked id ==> " + this.cardOne);
+     // console.log("previous clicked id ==> " + this.cardTwo);
+      let element = document.getElementById(this.cardOne);
+      element.className = 'none';
+      element = document.getElementById(this.cardTwo);
+      element.className = 'none';
+      this.numberFlip = 0;
+    }
+    if(this.numberFlip == 0){ this.cardOne = num};
+    if(this.numberFlip == 1) { this.cardTwo = num};
+  
+    //console.log("class id is :  " + this.num);
     let element = document.getElementById(num);
     element.className = 'flip-card-inner';
+
+    this.numberFlip++;
+    console.log("number of flip===> " + this.numberFlip);
+
+  
    // this.cssString = 'flip-card-inner';
   // element.className = flip-card-inner';
     //this.num = num + 1;
-    this.flipCard = true;
-    console.log("CARD ID + " + this.num)
-    this.getClass();
+
+   
+    //this.getClass();
   }
   getClass(){
     /*
@@ -40,7 +61,7 @@ export class MemoryActionComponent implements OnInit {
     }
     */
 
-    console.log("get class is called ");
+    
     //return this.cssString;
     //'flip-card-inner';
   }
