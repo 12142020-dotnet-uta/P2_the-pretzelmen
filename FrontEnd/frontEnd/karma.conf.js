a635f121a5847b493c33dc6a06f36ee848c27df7
Tyler Cadena <tylercadena@alum.calarts.edu>
2021-01-31 00:00:13 -0800

    tests pass and pipeline is ready

--- FrontEnd/frontEnd/karma.conf.js
+++ FrontEnd/frontEnd/karma.conf.js
@@ -25,11 +25,12 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, 'dist', 'reports', 'coverage'),
+      dir: require('path').join(__dirname, 'coverage'),
+      subdir: '.',
       reporters: [
-        { type: 'html', subdir: 'report-html' },
-        { type: 'lcovonly', subdir: 'report-lcovonly' },
-        { type: 'cobertura', subdir: 'report-cobertura' }
+        { type: 'html'  },
+        { type: 'text-summary' },
+        { type: 'cobertura' }
       ],
       fixWebpackSourcePaths: true
     },
--- FrontEnd/frontEnd/src/app/signup/signup.component.spec.ts
+++ FrontEnd/frontEnd/src/app/signup/signup.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { Location } from '@angular/common';
+import { PlayerService } from '../player.service';
 import { SignupComponent } from './signup.component';
 
 describe('SignupComponent', () => {
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
+  let mockLocation;
 
   beforeEach(async () => {
+    mockLocation = jasmine.createSpyObj('Location', ['back']);
     await TestBed.configureTestingModule({
-      declarations: [ SignupComponent ]
+      imports: [HttpClientTestingModule],
+      declarations: [SignupComponent],
+      providers: [
+        PlayerService,
+        { provider: Location, useValue: mockLocation }
+      ]
     })
     .compileComponents();
   });
