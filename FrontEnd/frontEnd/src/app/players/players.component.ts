c875d3900812c87aa7406e86d429be31ee8ea3af
Daniel Geiser <danielgeiser87@gmail.com>
2021-02-01 13:29:59 -0800

    web only on call to api

--- FrontEnd/frontEnd/src/app/players/players.component.ts
+++ FrontEnd/frontEnd/src/app/players/players.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PlayerViewModel } from '../player-view-model';
-import { UserService } from '../user.service';
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'app-players',
@@ -11,7 +11,7 @@ export class PlayersComponent implements OnInit {
   playerList: PlayerViewModel[];
   selectedPlayer: PlayerViewModel = new PlayerViewModel();
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: PlayerService) { }
 
   ngOnInit(): void {
     this.userService.PlayerList().subscribe(x => this.playerList = x);
