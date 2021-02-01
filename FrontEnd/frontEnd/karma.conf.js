270f067f59a52956484566ecfd9204e4ed2cfaf1
Tyler Cadena <tylercadena@alum.calarts.edu>
2021-02-01 01:37:20 -0800

    changes to karma & sonar properties

--- FrontEnd/frontEnd/karma.conf.js
+++ FrontEnd/frontEnd/karma.conf.js
@@ -30,6 +30,7 @@ module.exports = function (config) {
       reporters: [
         { type: 'html'  },
         { type: 'text-summary' },
+        { type: 'lcov' },
         { type: 'cobertura' }
       ],
       fixWebpackSourcePaths: true
